56b76fbd61a1ff8cf29300b9cea0a38c657e47a7
luislortega <a18016323@alumnos.uady.mx>
2021-02-07 11:23:11 -0500

    Base project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Piero_App
-💀 Flash component for web browsers made with Electron.JS
+# steps to reproduce the glitch
+
+- yarn
+- yarn build -w
+- launch testing_package.exe from /release/win-unpacked folder 
+(observe glitch) when the app starts and the dll is loaded, 2 termianl windows are appear for about half of second
+
+- compare to yarn start
+no terminal windows appear
---
+++ main.js
@@ -0,0 +1,51 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow(
+    {
+      width: 800,
+      height: 600,
+      webPreferences: {
+        plugins: true,
+        nodeIntegration: false
+      },
+     // frame: false,
+    })
+    mainWindow.setMenu(null)
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+ mainWindow.maximize();
+   mainWindow.show();
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+// app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname + '/lib/pepflashplayer.dll'))
+
+app.commandLine.appendSwitch('ppapi-flash-path', path.join((__dirname.includes(".asar") ? process.resourcesPath : __dirname) + '/app/lib/pepflashplayer.dll'))
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
